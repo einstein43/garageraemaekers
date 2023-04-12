e463f590a033ec8a60bdcf5ba6e7ec4eb9c8c911
Alexander <alexvanheteren@gmail.com>
2023-04-12 13:32:53 +0200

    tesstje

--- components/organisms/service.organism.tsx
+++ components/organisms/service.organism.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 import { Montserrat } from 'next/font/google'
-import styles from './services.module.css'
+import styles from '../../styles/organisms/services.module.css'
 
 const inter = Montserrat({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className={styles.main}>
-       
+       <h1>test</h1>
     </main>
   )
 }
