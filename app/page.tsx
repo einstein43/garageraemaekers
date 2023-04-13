a710f7d57f8138ac8bc1290d871656af2ada78cc
MikeVerlinde <64143584+MikeVerlinde@users.noreply.github.com>
2023-04-13 11:48:01 +0200

    Responsive navbar & footer

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 import Service from '../components/organisms/service.organism'
+import { Banner } from '@/components/molecules/banner.component'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-       <Service></Service>
+    <main id={styles.main}>
+       <Banner/>
     </main>
   )
 }
--- components/organisms/navBar.component.tsx
+++ components/organisms/navBar.component.tsx
@@ -1,11 +1,35 @@
+"use client"
+
 import styles from '../../styles/organisms/navBar.module.css'
 import { Button } from '../atoms/button.component'
 import phone from '../../public/phone.svg'
 import location from '../../public/location.svg'
 import Image from 'next/image'
 import logo from '../../public/logo_text.svg'
+import HamburgerBtn from '../atoms/hamburgerBtn.component'
+import { useState } from 'react'
+import classNames from 'classnames'
 
 export const NavBar = () => {
+
+    // State
+    const [menuOpen, setMenuOpen] = useState<boolean>(false)
+
+    // Fn - handle click
+    const handleClick = (id: string) => {
+        
+        // Get element
+        const el = document.getElementById(id)
+
+        // If element exists
+        if (el) {
+
+            // Scroll to element
+            el.scrollIntoView({
+                behavior: 'smooth'
+            })
+        }
+    }
 
     return (
         <div id={styles.container}>
@@ -22,10 +46,10 @@ export const NavBar = () => {
 
             {/* Links */}
             <ul id={styles.linkContainer}>
-                <li className={styles.link}>Home</li>
-                <li className={styles.link}>Diensten</li>
-                <li className={styles.link}>Reviews</li>
-                <li className={styles.link}>Contact</li>
+                <li className={styles.link} onClick={() => handleClick('section-1')}>Home</li>
+                <li className={styles.link} onClick={() => handleClick('section-1')}>Diensten</li>
+                <li className={styles.link} onClick={() => handleClick('section-1')}>Reviews</li>
+                <li className={styles.link} onClick={() => handleClick('section-1')}>Contact</li>
             </ul>
 
             {/* Buttons */}
@@ -47,6 +71,28 @@ export const NavBar = () => {
                     }}
                 >Bellen</Button>
             </div>
+
+            {/* Hamburger button */}
+            <div id={styles.hamburgerBtnWrap}>
+                <HamburgerBtn style='style1' isOpen={menuOpen} setIsOpen={setMenuOpen}/>
+            </div>
+
+            {/* Mobile menu */}
+            {
+                <div
+                    className={classNames(
+                        styles.mobileMenu,
+                        menuOpen && styles.mobileMenu__state_open
+                    )}
+                >
+                    <ul id={styles.mobileLinkContainer}>
+                        <li className={styles.mobileLink} onClick={() => handleClick('section-1')}>Home</li>
+                        <li className={styles.mobileLink} onClick={() => handleClick('section-1')}>Diensten</li>
+                        <li className={styles.mobileLink} onClick={() => handleClick('section-1')}>Reviews</li>
+                        <li className={styles.mobileLink} onClick={() => handleClick('section-1')}>Contact</li>
+                    </ul>
+                </div>
+            }
         </div>
     )
 }
