26347c1f9d3b6a969909870f70231e6d71bca268
MikeVerlinde <64143584+MikeVerlinde@users.noreply.github.com>
2023-04-13 13:06:14 +0200

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
-import Service from '../components/organisms/service.organism'
 import { Banner } from '@/components/molecules/banner.component'
+import { Footer } from '@/components/organisms/footer.component'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main id={styles.main}>
-       <Banner/>
-    </main>
+    <>
+      <main id={styles.main}>
+        <Banner/>
+      </main>
+      <Footer/>
+    </>
   )
 }
---
+++ components/organisms/footer.component.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import Image from 'next/image'
+import styles from '../../styles/organisms/footer.module.css'
+import logo from '../../public/logo_text.svg'
+
+export const Footer = () => {
+
+    // Fn - handle click
+    const handleClick = (id: string) => {
+        
+        // Get element
+        const el = document.getElementById(id)
+
+        // If element exists
+        if (el) {
+
+            // Scroll to element
+            el.scrollIntoView({
+                behavior: 'smooth'
+            })
+        }
+    }
+
+    // Current year
+    const year = new Date().getFullYear()
+
+    return (
+        <>
+            <div id={styles.footer}>
+
+                {/* Logo */}
+                <div id={styles.logoWrap}>
+                    <Image
+                        src={logo}
+                        alt='Garage Raemaekers logo'
+                        fill={true}
+                        id={styles.logo}
+                    />
+                </div>
+
+                {/* Details */}
+                <div className={styles.section}>
+                    <p className={styles.title}>Garage Raemaekers</p>
+                    <ul className={styles.ul}>
+                        <li className={styles.li}><a href='https://goo.gl/maps/pgG1u7PspoTZ7Nc19' target='_blank'>Visdijk 9<br/>6034 SZ, Nederweert-eind</a></li>
+                        <li className={styles.li}><a href="tel:061234567">T: +31 6 12345678</a></li>
+                    </ul>
+                </div>
+
+                {/* Sections */}
+                <div className={styles.section}>
+                    <p className={styles.title}>Secties</p>
+                    <ul className={styles.ul}>
+                        <li className={styles.li} onClick={() => handleClick('section-1')}>Home</li>
+                        <li className={styles.li} onClick={() => handleClick('section-1')}>Diensten</li>
+                        <li className={styles.li} onClick={() => handleClick('section-1')}>Reviews</li>
+                        <li className={styles.li} onClick={() => handleClick('section-1')}>Contact</li>
+                    </ul>
+                </div>
+            </div>
+
+            {/* Disclosure bar */}
+            <div id={styles.disclosureContainer}>
+                <p>Copyright @ {year} Garage Raemaekers</p>
+            </div>
+        </>
+    )
+}
