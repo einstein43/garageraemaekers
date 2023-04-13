f59a0f74a79d654b84e7cf7136f4a2554c82faf4
Alexander <alexvanheteren@gmail.com>
2023-04-13 13:05:26 +0200

    location klaar

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,13 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 import Service from '../components/organisms/service.organism'
-import Reviews from '@/components/organisms/reviews.organism'
+import Reviews from '../components/organisms/reviews.organism'
+import Location from '@/components/organisms/location.organism'
  
 export default function Home() {
   return (
     <main id={styles.main}>
-       <Reviews></Reviews>
-    
-      
+       <Location></Location>
     </main>
   )
 }
