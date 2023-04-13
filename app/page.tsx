3277a395529ec873024737d19a5465e57f080189
MikeVerlinde <64143584+MikeVerlinde@users.noreply.github.com>
2023-04-13 16:23:09 +0200

    Reviews responsive

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
     <>
       <main id={styles.main}>
         <Banner/>
-        <Service/>
+        {/* <Service/> */}
         <Reviews/>
-        <Location/>
+        {/* <Location/> */}
       </main>
       <Footer/>
     </>
